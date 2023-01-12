60b991c80552536be997fe72ba9275acbf94ad1a
mghashim <35562847+mghashim@users.noreply.github.com>
2023-01-12 15:13:30 +0000

    Create frequency.py

---
+++ frequency.py
@@ -0,0 +1,113 @@
+ph="You can have data without information, but you cannot have information without data"
+phr=ph.lower()
+phrlenght=int(len(phr))
+#print(phr)
+#print(phrlenght)
+letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+letlenght=int(len(letters))
+fa=0
+fb=0
+fc=0
+fd=0
+fe=0
+ff=0
+fg=0
+fh=0
+fi=0
+fj=0
+fk=0
+fl=0
+fm=0
+fn=0
+fo=0
+fp=0
+fq=0
+fr=0
+fs=0
+ft=0
+fu=0
+fv=0
+fw=0
+fx=0
+fy=0
+fz=0
+for i in range(0,phrlenght):
+  i+=1
+  if (phr[i]==letters[0]):
+   fa+=1
+  if (phr[i]==letters[1]):
+   fb+=1
+  if (phr[i]==letters[2]):
+   fc+=1
+  if (phr[i]==letters[3]):
+   fd+=1
+  if (phr[i]==letters[4]):
+   fe+=1
+  if (phr[i]==letters[5]):
+   ff+=1
+  if (phr[i]==letters[6]):
+   fg+=1
+  if (phr[i]==letters[7]):
+   fh+=1
+  if (phr[i]==letters[8]):
+   fi+=1
+  if (phr[i]==letters[9]):
+   fj+=1
+  if (phr[i]==letters[10]):
+   fk+=1
+  if (phr[i]==letters[11]):
+   fl+=1
+  if (phr[i]==letters[12]):
+   fm+=1
+  if (phr[i]==letters[13]):
+   fn+=1 
+  if (phr[i]==letters[14]):
+   fo+=1
+  if (phr[i]==letters[15]):
+   fp+=1
+  if (phr[i]==letters[16]):
+   fq+=1
+  if (phr[i]==letters[17]):
+   fr+=1
+  if (phr[i]==letters[18]):
+   fs+=1
+  if (phr[i]==letters[19]):
+   ft+=1
+  if (phr[i]==letters[20]):
+   fu+=1
+  if (phr[i]==letters[21]):
+   fv+=1
+  if (phr[i]==letters[22]):
+   fw+=1
+  if (phr[i]==letters[23]):
+   fx+=1
+  if (phr[i]==letters[24]):
+   fy+=1
+  if (phr[i]==letters[25]):
+   fz+=1
+  print(fa) 
+  print(fb) 
+  print(fc) 
+  print(fd) 
+  print(fe) 
+  print(ff) 
+  print(fg) 
+  print(fh) 
+  print(fi) 
+  print(fj)
+  print(fk) 
+  print(fl) 
+  print(fm) 
+  print(fn) 
+  print(fo) 
+  print(fp) 
+  print(fq) 
+  print(fr) 
+  print(fs) 
+  print(ft) 
+  print(fu)
+  print(fv) 
+  print(fw) 
+  print(fx) 
+  print(fy) 
+  print(fz) 
